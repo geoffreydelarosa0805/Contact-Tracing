19d5f7bb7bd43c7d8c52d05a35ebadeb12316001
Geoffrey Dela Rosa <delarosageoff@gmail.com>
2022-06-10 02:37:21 +0800

    Insert Label and TextBox

--- Contact_Tracing/Form1.Designer.cs
+++ Contact_Tracing/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Contact_Tracing
 {
-    partial class Form1
+    partial class Form_JowaApplicationForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,11 +29,31 @@
         private void InitializeComponent()
         {
             this.buttonSubmit = new System.Windows.Forms.Button();
+            this.label_FirstName = new System.Windows.Forms.Label();
+            this.textBox_FirstName = new System.Windows.Forms.TextBox();
+            this.label_Surname = new System.Windows.Forms.Label();
+            this.label_Age = new System.Windows.Forms.Label();
+            this.label_Birthdate = new System.Windows.Forms.Label();
+            this.label_Address = new System.Windows.Forms.Label();
+            this.label_Status = new System.Windows.Forms.Label();
+            this.label_Height = new System.Windows.Forms.Label();
+            this.label_Weight = new System.Windows.Forms.Label();
+            this.textBox_Surname = new System.Windows.Forms.TextBox();
+            this.textBox_Age = new System.Windows.Forms.TextBox();
+            this.textBox_Birthdate = new System.Windows.Forms.TextBox();
+            this.textBox_Address = new System.Windows.Forms.TextBox();
+            this.textBox_Status = new System.Windows.Forms.TextBox();
+            this.textBox_Height = new System.Windows.Forms.TextBox();
+            this.textBox_Weight = new System.Windows.Forms.TextBox();
+            this.groupBox_PersonalInformation = new System.Windows.Forms.GroupBox();
+            this.textBox_Gender = new System.Windows.Forms.TextBox();
+            this.label_Gender = new System.Windows.Forms.Label();
+            this.groupBox_PersonalInformation.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonSubmit
             // 
-            this.buttonSubmit.Location = new System.Drawing.Point(247, 321);
+            this.buttonSubmit.Location = new System.Drawing.Point(638, 410);
             this.buttonSubmit.Name = "buttonSubmit";
             this.buttonSubmit.Size = new System.Drawing.Size(75, 23);
             this.buttonSubmit.TabIndex = 0;
@@ -41,14 +61,198 @@
             this.buttonSubmit.UseVisualStyleBackColor = true;
             this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
             // 
-            // Form1
+            // label_FirstName
+            // 
+            this.label_FirstName.AutoSize = true;
+            this.label_FirstName.Font = new System.Drawing.Font("Felix Titling", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_FirstName.Location = new System.Drawing.Point(42, 35);
+            this.label_FirstName.Name = "label_FirstName";
+            this.label_FirstName.Size = new System.Drawing.Size(83, 14);
+            this.label_FirstName.TabIndex = 1;
+            this.label_FirstName.Text = "First Name :";
+            // 
+            // textBox_FirstName
+            // 
+            this.textBox_FirstName.Location = new System.Drawing.Point(143, 31);
+            this.textBox_FirstName.Name = "textBox_FirstName";
+            this.textBox_FirstName.Size = new System.Drawing.Size(192, 29);
+            this.textBox_FirstName.TabIndex = 2;
+            // 
+            // label_Surname
+            // 
+            this.label_Surname.AutoSize = true;
+            this.label_Surname.Font = new System.Drawing.Font("Felix Titling", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_Surname.Location = new System.Drawing.Point(42, 74);
+            this.label_Surname.Name = "label_Surname";
+            this.label_Surname.Size = new System.Drawing.Size(73, 14);
+            this.label_Surname.TabIndex = 3;
+            this.label_Surname.Text = "Surname :";
+            // 
+            // label_Age
+            // 
+            this.label_Age.AutoSize = true;
+            this.label_Age.Font = new System.Drawing.Font("Felix Titling", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_Age.Location = new System.Drawing.Point(42, 179);
+            this.label_Age.Name = "label_Age";
+            this.label_Age.Size = new System.Drawing.Size(36, 14);
+            this.label_Age.TabIndex = 4;
+            this.label_Age.Text = "Age :";
+            // 
+            // label_Birthdate
+            // 
+            this.label_Birthdate.AutoSize = true;
+            this.label_Birthdate.Font = new System.Drawing.Font("Felix Titling", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_Birthdate.Location = new System.Drawing.Point(42, 143);
+            this.label_Birthdate.Name = "label_Birthdate";
+            this.label_Birthdate.Size = new System.Drawing.Size(80, 14);
+            this.label_Birthdate.TabIndex = 5;
+            this.label_Birthdate.Text = "Birthdate :";
+            // 
+            // label_Address
+            // 
+            this.label_Address.AutoSize = true;
+            this.label_Address.Font = new System.Drawing.Font("Felix Titling", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_Address.Location = new System.Drawing.Point(42, 214);
+            this.label_Address.Name = "label_Address";
+            this.label_Address.Size = new System.Drawing.Size(69, 14);
+            this.label_Address.TabIndex = 6;
+            this.label_Address.Text = "Address :";
+            // 
+            // label_Status
+            // 
+            this.label_Status.AutoSize = true;
+            this.label_Status.Font = new System.Drawing.Font("Felix Titling", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_Status.Location = new System.Drawing.Point(42, 319);
+            this.label_Status.Name = "label_Status";
+            this.label_Status.Size = new System.Drawing.Size(55, 14);
+            this.label_Status.TabIndex = 7;
+            this.label_Status.Text = "Status :";
+            // 
+            // label_Height
+            // 
+            this.label_Height.AutoSize = true;
+            this.label_Height.Font = new System.Drawing.Font("Felix Titling", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_Height.Location = new System.Drawing.Point(42, 248);
+            this.label_Height.Name = "label_Height";
+            this.label_Height.Size = new System.Drawing.Size(56, 14);
+            this.label_Height.TabIndex = 8;
+            this.label_Height.Text = "Height :";
+            // 
+            // label_Weight
+            // 
+            this.label_Weight.AutoSize = true;
+            this.label_Weight.Font = new System.Drawing.Font("Felix Titling", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_Weight.Location = new System.Drawing.Point(42, 283);
+            this.label_Weight.Name = "label_Weight";
+            this.label_Weight.Size = new System.Drawing.Size(59, 14);
+            this.label_Weight.TabIndex = 9;
+            this.label_Weight.Text = "Weight :";
+            // 
+            // textBox_Surname
+            // 
+            this.textBox_Surname.Location = new System.Drawing.Point(143, 66);
+            this.textBox_Surname.Name = "textBox_Surname";
+            this.textBox_Surname.Size = new System.Drawing.Size(192, 29);
+            this.textBox_Surname.TabIndex = 10;
+            // 
+            // textBox_Age
+            // 
+            this.textBox_Age.Location = new System.Drawing.Point(143, 171);
+            this.textBox_Age.Name = "textBox_Age";
+            this.textBox_Age.Size = new System.Drawing.Size(192, 29);
+            this.textBox_Age.TabIndex = 11;
+            // 
+            // textBox_Birthdate
+            // 
+            this.textBox_Birthdate.Location = new System.Drawing.Point(143, 136);
+            this.textBox_Birthdate.Name = "textBox_Birthdate";
+            this.textBox_Birthdate.Size = new System.Drawing.Size(192, 29);
+            this.textBox_Birthdate.TabIndex = 12;
+            // 
+            // textBox_Address
+            // 
+            this.textBox_Address.Location = new System.Drawing.Point(143, 206);
+            this.textBox_Address.Name = "textBox_Address";
+            this.textBox_Address.Size = new System.Drawing.Size(192, 29);
+            this.textBox_Address.TabIndex = 13;
+            // 
+            // textBox_Status
+            // 
+            this.textBox_Status.Location = new System.Drawing.Point(143, 311);
+            this.textBox_Status.Name = "textBox_Status";
+            this.textBox_Status.Size = new System.Drawing.Size(192, 29);
+            this.textBox_Status.TabIndex = 14;
+            // 
+            // textBox_Height
+            // 
+            this.textBox_Height.Location = new System.Drawing.Point(143, 241);
+            this.textBox_Height.Name = "textBox_Height";
+            this.textBox_Height.Size = new System.Drawing.Size(192, 29);
+            this.textBox_Height.TabIndex = 15;
+            // 
+            // textBox_Weight
+            // 
+            this.textBox_Weight.Location = new System.Drawing.Point(143, 276);
+            this.textBox_Weight.Name = "textBox_Weight";
+            this.textBox_Weight.Size = new System.Drawing.Size(192, 29);
+            this.textBox_Weight.TabIndex = 16;
+            // 
+            // groupBox_PersonalInformation
+            // 
+            this.groupBox_PersonalInformation.Controls.Add(this.textBox_Gender);
+            this.groupBox_PersonalInformation.Controls.Add(this.textBox_Height);
+            this.groupBox_PersonalInformation.Controls.Add(this.label_Height);
+            this.groupBox_PersonalInformation.Controls.Add(this.label_Gender);
+            this.groupBox_PersonalInformation.Controls.Add(this.label_Status);
+            this.groupBox_PersonalInformation.Controls.Add(this.label_Weight);
+            this.groupBox_PersonalInformation.Controls.Add(this.textBox_Weight);
+            this.groupBox_PersonalInformation.Controls.Add(this.label_Address);
+            this.groupBox_PersonalInformation.Controls.Add(this.textBox_Birthdate);
+            this.groupBox_PersonalInformation.Controls.Add(this.textBox_Surname);
+            this.groupBox_PersonalInformation.Controls.Add(this.label_Birthdate);
+            this.groupBox_PersonalInformation.Controls.Add(this.label_FirstName);
+            this.groupBox_PersonalInformation.Controls.Add(this.textBox_Age);
+            this.groupBox_PersonalInformation.Controls.Add(this.textBox_Status);
+            this.groupBox_PersonalInformation.Controls.Add(this.label_Age);
+            this.groupBox_PersonalInformation.Controls.Add(this.textBox_FirstName);
+            this.groupBox_PersonalInformation.Controls.Add(this.label_Surname);
+            this.groupBox_PersonalInformation.Controls.Add(this.textBox_Address);
+            this.groupBox_PersonalInformation.Font = new System.Drawing.Font("Curlz MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox_PersonalInformation.Location = new System.Drawing.Point(12, 56);
+            this.groupBox_PersonalInformation.Name = "groupBox_PersonalInformation";
+            this.groupBox_PersonalInformation.Size = new System.Drawing.Size(380, 366);
+            this.groupBox_PersonalInformation.TabIndex = 17;
+            this.groupBox_PersonalInformation.TabStop = false;
+            this.groupBox_PersonalInformation.Text = "Personal Information";
+            // 
+            // textBox_Gender
+            // 
+            this.textBox_Gender.Location = new System.Drawing.Point(143, 101);
+            this.textBox_Gender.Name = "textBox_Gender";
+            this.textBox_Gender.Size = new System.Drawing.Size(192, 29);
+            this.textBox_Gender.TabIndex = 18;
+            // 
+            // label_Gender
+            // 
+            this.label_Gender.AutoSize = true;
+            this.label_Gender.Font = new System.Drawing.Font("Felix Titling", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_Gender.Location = new System.Drawing.Point(42, 109);
+            this.label_Gender.Name = "label_Gender";
+            this.label_Gender.Size = new System.Drawing.Size(65, 14);
+            this.label_Gender.TabIndex = 17;
+            this.label_Gender.Text = "Gender :";
+            // 
+            // Form_JowaApplicationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 356);
+            this.ClientSize = new System.Drawing.Size(740, 454);
+            this.Controls.Add(this.groupBox_PersonalInformation);
             this.Controls.Add(this.buttonSubmit);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "Form_JowaApplicationForm";
+            this.Text = "Jowa Application Form";
+            this.groupBox_PersonalInformation.ResumeLayout(false);
+            this.groupBox_PersonalInformation.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -56,5 +260,24 @@
         #endregion
 
         private Button buttonSubmit;
+        private Label label_FirstName;
+        private TextBox textBox_FirstName;
+        private Label label_Surname;
+        private Label label_Age;
+        private Label label_Birthdate;
+        private Label label_Address;
+        private Label label_Status;
+        private Label label_Height;
+        private Label label_Weight;
+        private TextBox textBox_Surname;
+        private TextBox textBox_Age;
+        private TextBox textBox_Birthdate;
+        private TextBox textBox_Address;
+        private TextBox textBox_Status;
+        private TextBox textBox_Height;
+        private TextBox textBox_Weight;
+        private GroupBox groupBox_PersonalInformation;
+        private TextBox textBox_Gender;
+        private Label label_Gender;
     }
 }
--- Contact_Tracing/Form1.cs
+++ Contact_Tracing/Form1.cs
@@ -1,8 +1,8 @@
 namespace Contact_Tracing
 {
-    public partial class Form1 : Form
+    public partial class Form_JowaApplicationForm : Form
     {
-        public Form1()
+        public Form_JowaApplicationForm()
         {
             InitializeComponent();
         }
@@ -10,7 +10,17 @@ namespace Contact_Tracing
         private void buttonSubmit_Click(object sender, EventArgs e)
         {
             StreamWriter file = new StreamWriter(@"C:\Users\HP\Documents\Outputs\Jowa Application Form.text");
-            file.WriteLine("Pogi Ako");
+            file.WriteLine("First Name : " + textBox_FirstName.Text);
+            file.WriteLine("Surname : " + textBox_Surname.Text);
+            file.WriteLine("Gender : " + textBox_Gender.Text);
+            file.WriteLine("Birthdate : " + textBox_Birthdate.Text);
+            file.WriteLine("Age : " + textBox_Age.Text);
+            file.WriteLine("Address : " + textBox_Address.Text);
+            file.WriteLine("Height : " + textBox_Height.Text);
+            file.WriteLine("Weight : " + textBox_Weight.Text);
+            file.WriteLine("Status : " + textBox_Status.Text);
+           
+
             file.Close();
         }
 
