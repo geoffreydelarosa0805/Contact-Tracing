f00c0da36d87da8b45b2f52e1489cf0c7838092e
Geoffrey Dela Rosa <delarosageoff@gmail.com>
2022-06-10 18:09:35 +0800

    Final Project UPDATED

--- Contact_Tracing/Form1.cs
+++ Contact_Tracing/Form1.cs
@@ -9,7 +9,7 @@ namespace Contact_Tracing
 
         private void buttonSubmit_Click(object sender, EventArgs e)
         {
-            StreamWriter file = new StreamWriter(@"C:\Users\HP\Documents\Outputs\Jowa Application Form.text");
+            StreamWriter file = new StreamWriter(@"C:\Users\HP\Documents\Outputs\Contact Tracing Form.text");
             file.WriteLine("First Name : " + textBox_FirstName.Text);
             file.WriteLine("Surname : " + textBox_Surname.Text);
             file.WriteLine("Gender : " + textBox_Gender.Text);
@@ -19,6 +19,8 @@ namespace Contact_Tracing
             file.WriteLine("Email : " + textBox_Email.Text);
             file.WriteLine("Phone No. : " + textBox_PhoneNo.Text);
             file.WriteLine("Telephone : " + textBox_Telephone.Text);
+            file.WriteLine("");
+            file.WriteLine("Other Person to Contact");
             file.WriteLine("Person's Name : " + textBox_PersonName.Text);
             file.WriteLine("Relationship : " + textBox_Relationship.Text);
             file.WriteLine("Email Address : " + textBox_EmailAddress.Text);
